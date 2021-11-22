23c06a4c4fbbfdcc0375e5d6c02e5f238be8d198
Paul Fentress <fentresspaul@berkeley.edu>
2021-11-22 01:35:17 -0800

    fixing indentation.

--- app.py
+++ app.py
@@ -209,36 +209,36 @@ START OF MODEL
 """
 
 
-  import glob
-  import seaborn as sns
-  import subprocess
-  import matplotlib.pyplot as plt
-  import seaborn as sns
-  import numpy as np
-  import librosa
-  import plotly.graph_objs as go
+import glob
+import seaborn as sns
+import subprocess
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import librosa
+import plotly.graph_objs as go
 
-  # Importing the Data
-  data = pd.read_csv("Mentia_Data_Updated_nov20.csv").drop("Unnamed: 0", axis=1)
-  matrix = pd.read_csv("Audio_Features_updated_nov20.csv").drop("Unnamed: 0", axis=1)
-  labels = pd.read_csv("Labels_updated_nov20.csv")["Labels"]
-
-
-
-  # print(data.shape, matrix.shape, labels.shape)
+# Importing the Data
+data = pd.read_csv("Mentia_Data_Updated_nov20.csv").drop("Unnamed: 0", axis=1)
+matrix = pd.read_csv("Audio_Features_updated_nov20.csv").drop("Unnamed: 0", axis=1)
+labels = pd.read_csv("Labels_updated_nov20.csv")["Labels"]
 
 
-  # Importing ML Dependencies
-  import sklearn as sk
-  from sklearn import preprocessing
-  from tensorflow.keras.layers import Conv2D,MaxPool2D, Flatten, LSTM
-  from keras.layers import Dropout,Dense,TimeDistributed
-  from keras.models import Sequential
-  from tensorflow.keras.utils import to_categorical
-  from sklearn.utils.class_weight import compute_class_weight
-  from sklearn.model_selection import train_test_split
-  from sklearn.neural_network import MLPClassifier
-  from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# print(data.shape, matrix.shape, labels.shape)
+
+
+# Importing ML Dependencies
+import sklearn as sk
+from sklearn import preprocessing
+from tensorflow.keras.layers import Conv2D,MaxPool2D, Flatten, LSTM
+from keras.layers import Dropout,Dense,TimeDistributed
+from keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 
 
@@ -247,111 +247,111 @@ START OF MODEL
   # MM represents the min max values. This is used to normalize the data.
 
 
-  """
-  This is the MLP model. We have tried using with and without normalization,
-  and are getting similar results. Might need to train on more data, to get a more
-  representative sample.
-  """
+"""
+This is the MLP model. We have tried using with and without normalization,
+and are getting similar results. Might need to train on more data, to get a more
+representative sample.
+"""
 
-  data = pd.read_csv("Mentia_Data_Updated_nov20.csv").drop("Unnamed: 0", axis=1)
-  matrix = pd.read_csv("Audio_Features_updated_nov20.csv").drop("Unnamed: 0", axis=1)
-  labels = pd.read_csv("Labels_updated_nov20.csv")["Labels"]
+data = pd.read_csv("Mentia_Data_Updated_nov20.csv").drop("Unnamed: 0", axis=1)
+matrix = pd.read_csv("Audio_Features_updated_nov20.csv").drop("Unnamed: 0", axis=1)
+labels = pd.read_csv("Labels_updated_nov20.csv")["Labels"]
 
-  seed = 9
+seed = 9
 
-  # Here is the design matrix with all of the audio features.
-  # MM represents the min max values. This is used to normalize the data.
-  MM = (dataset_minmax(matrix.values))
+# Here is the design matrix with all of the audio features.
+# MM represents the min max values. This is used to normalize the data.
+MM = (dataset_minmax(matrix.values))
 
-  # X = normalize_dataset(matrix.values, MM)
-  X = matrix
-  # Labels: 1, 2, 3. Integers
-  Y = labels
+# X = normalize_dataset(matrix.values, MM)
+X = matrix
+# Labels: 1, 2, 3. Integers
+Y = labels
 
-  # Shuffling the data so no patters get picked up in the algorithm about how the data is organized in the table.
-  sk.utils.shuffle(X, random_state=seed)
+# Shuffling the data so no patters get picked up in the algorithm about how the data is organized in the table.
+sk.utils.shuffle(X, random_state=seed)
 
-  # Creating training, Testing tables for the design matrix and the corresponding labels
-  X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = .1, random_state = seed)
+# Creating training, Testing tables for the design matrix and the corresponding labels
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = .1, random_state = seed)
 
-  # Instantiating the Model
-  model=MLPClassifier(alpha=0.0001, batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=500, momentum=0.7, random_state=seed)
-  model.fit(X_train,Y_train)
+# Instantiating the Model
+model=MLPClassifier(alpha=0.0001, batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=500, momentum=0.7, random_state=seed)
+model.fit(X_train,Y_train)
 
-  # Making Predictions on the Test Set
-  y_pred = model.predict(X_test)
+# Making Predictions on the Test Set
+y_pred = model.predict(X_test)
 
-  # Get the classification accuracy on the test set
-  KNN_Y_test = Y_test.astype('int')
-  print("MLP Neural Network")
-  print(classification_report(KNN_Y_test, y_pred))
+# Get the classification accuracy on the test set
+KNN_Y_test = Y_test.astype('int')
+print("MLP Neural Network")
+print(classification_report(KNN_Y_test, y_pred))
 
-  print(confusion_matrix(KNN_Y_test, y_pred))
+print(confusion_matrix(KNN_Y_test, y_pred))
 
-  """
-  Trying different models.
-  """
+"""
+Trying different models.
+"""
 
-  MM = (dataset_minmax(matrix.values))
-  X = normalize_dataset(matrix.values, MM)
-  seed = 9
-  Y = labels
+MM = (dataset_minmax(matrix.values))
+X = normalize_dataset(matrix.values, MM)
+seed = 9
+Y = labels
 
-  X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = .1, random_state = seed)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = .1, random_state = seed)
 
-  from sklearn.decomposition import PCA
-  pca = PCA(n_components=131)
+from sklearn.decomposition import PCA
+pca = PCA(n_components=131)
 
-  # Fitting the PCA model to the training data
-  X_train = pca.fit_transform(X_train)
+# Fitting the PCA model to the training data
+X_train = pca.fit_transform(X_train)
 
-  # transofmring the test data
-  X_test = pca.transform(X_test)
+# transofmring the test data
+X_test = pca.transform(X_test)
 
-  # Using KNN Clustering to make predictions
-  from sklearn.ensemble import RandomForestClassifier
-  from sklearn.datasets import make_classification
-  from sklearn.linear_model import *
+# Using KNN Clustering to make predictions
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.linear_model import *
 
-  clf = RandomForestClassifier(max_depth=2, random_state=0)
-  clf.fit(X_train, Y_train)
-  y_pred = clf.predict(X_test)
-  print("Random Forrest")
-  print(classification_report(Y_test, y_pred))
-  print(confusion_matrix(Y_test, y_pred))
-
-
-  """
-  KNN Classifier
-  """
-  # k = np.sqrt(data.shape[0])
-  k = 11
-  print("k " + str(k))
-
-  from sklearn.neighbors import KNeighborsClassifier as knn
-  clf = knn(n_neighbors=k)
-  KNeighborsClassifier = clf.fit(X_train, Y_train)
-
-  y_pred = KNeighborsClassifier.predict(X_test)
-
-  # Get the classification accuracy on the test data
-  KNN_Y_test = Y_test.astype('int')
-  print("KNN")
-  print(classification_report(KNN_Y_test, y_pred))
-  print(confusion_matrix(KNN_Y_test, y_pred))
+clf = RandomForestClassifier(max_depth=2, random_state=0)
+clf.fit(X_train, Y_train)
+y_pred = clf.predict(X_test)
+print("Random Forrest")
+print(classification_report(Y_test, y_pred))
+print(confusion_matrix(Y_test, y_pred))
 
 
+"""
+KNN Classifier
+"""
+# k = np.sqrt(data.shape[0])
+k = 11
+print("k " + str(k))
+
+from sklearn.neighbors import KNeighborsClassifier as knn
+clf = knn(n_neighbors=k)
+KNeighborsClassifier = clf.fit(X_train, Y_train)
+
+y_pred = KNeighborsClassifier.predict(X_test)
+
+# Get the classification accuracy on the test data
+KNN_Y_test = Y_test.astype('int')
+print("KNN")
+print(classification_report(KNN_Y_test, y_pred))
+print(confusion_matrix(KNN_Y_test, y_pred))
 
 
 
 
 
-  """
-  Below are helper functions that are connected to the record buton.
 
-  """
 
-  def extract_feature(audio, mfcc=True, chroma=True, mel=True):
+"""
+Below are helper functions that are connected to the record buton.
+
+"""
+
+def extract_feature(audio, mfcc=True, chroma=True, mel=True):
     audio = np.asarray(audio)
     sample_rate = 8000
     if chroma:
@@ -374,11 +374,11 @@ START OF MODEL
     return np.array(result)
 
 
-  emotion_dict = {1: "Positive", 2: "Negative", 3: "Neutral"}
+emotion_dict = {1: "Positive", 2: "Negative", 3: "Neutral"}
 
-  from Helper import extract_feature, load_audio, noise_reducer
 
-  def make_prediction(file_path):
+
+def make_prediction(file_path):
     # X = norm_input(file_path, MM)
     # X = pca.transform(X)
     # y_pred = KNeighborsClassifier.predict(X)
@@ -415,10 +415,10 @@ from IPython.core.display import display as audio_display
 
 
 paths = ([
-        "Audio_Features_updated_nov20.csv",
-        "Labels_updated_nov20.csv",
-        "Mentia_Data_Updated_nov20.csv"
-        ])
+    "Audio_Features_updated_nov20.csv",
+    "Labels_updated_nov20.csv",
+    "Mentia_Data_Updated_nov20.csv"
+    ])
 
 # .drop("Unnamed:0", axis=1)
 data = pd.read_csv(paths[2]).drop("Unnamed: 0", axis=1)
@@ -485,6 +485,9 @@ if st.button("Record"):
 
 
     st.audio(Helper.read_audio(path_myrecording))
+
+
+print("Complete")
 
 
 
